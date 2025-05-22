8a8f71f0b7ee804f4939dfc09bc25144e53a269d
Hick <124798752+hick-hpe@users.noreply.github.com>
2025-05-22 18:24:08 +0000

    certificado

--- app.py
+++ app.py
@@ -2,8 +2,13 @@
 from flask import Flask, render_template, request, send_file
 import pdfkit
 import os
+import base64
 
 app = Flask(__name__)
+
+def imagem_para_base64(caminho):
+    with open(caminho, 'rb') as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
 
 @app.route('/')
 def index():
@@ -15,15 +20,18 @@ def gerar_certificado():
     curso = request.form['curso']
     carga_horaria = request.form['carga_horaria']
     data = request.form['data']
-    logo_url = os.path.join(os.getcwd(), 'static', 'logo.png')
-    print("logo_url:", logo_url)
+    logo_path = os.path.join(os.getcwd(), 'static', 'logo.png')
+    logo_base64 = imagem_para_base64(logo_path)
+    percentual_acertos=80
 
     rendered = render_template('certificado_pdf.html',
-                                nome=nome,
-                                curso=curso,
-                                carga_horaria=carga_horaria,
-                                data=data,
-                                logo_url=logo_url)
+                           nome=nome,
+                           curso=curso,
+                           carga_horaria=carga_horaria,
+                           data=data,
+                           logo_base64=logo_base64,
+                           percentual_acertos=percentual_acertos)
+
 
     pdf_path = f'certificado_{nome.replace(" ", "_")}.pdf'
     pdfkit.from_string(rendered, pdf_path, options={'enable-local-file-access': ''})
