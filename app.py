af3dda0b5b94dbaf08d0879f6c9d344b73a94031
Hick <124798752+hick-hpe@users.noreply.github.com>
2025-06-06 14:54:02 +0000

    Certificado de Django

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ from flask import Flask, render_template, request, send_file
 import pdfkit
 import os
 import base64
+from datetime import datetime
 
 app = Flask(__name__)
 
 def imagem_para_base64(caminho):
     with open(caminho, 'rb') as img_file:
         return base64.b64encode(img_file.read()).decode('utf-8')
+
+def data_extenso(data:datetime):
+    meses = [
+        "Janeiro", "Fevereiro", "Março", "Abril",
+        "Maio", "Junho", "Julho", "Agosto",
+        "Setembro", "Outubro", "Novembro", "Dezembro"
+    ]
+
+    return f"{data.day} de {meses[data.month]} de {data.year}"
 
 @app.route('/')
 def index():
@@ -17,21 +27,18 @@ def index():
 @app.route('/gerar_certificado', methods=['POST'])
 def gerar_certificado():
     nome = request.form['nome']
-    curso = request.form['curso']
-    carga_horaria = request.form['carga_horaria']
-    data = request.form['data']
+    # data = "2025-06-06"
+    data_datetime = datetime.today()#datetime.strptime(data, "%Y-%m-%d")
+    data_formatada = data_extenso(data_datetime)
     logo_path = os.path.join(os.getcwd(), 'static', 'logo.png')
     logo_base64 = imagem_para_base64(logo_path)
     percentual_acertos=80
 
     rendered = render_template('certificado_pdf.html',
-                           nome=nome,
-                           curso=curso,
-                           carga_horaria=carga_horaria,
-                           data=data,
+                           nome=nome.upper(),
+                           data=data_formatada,
                            logo_base64=logo_base64,
                            percentual_acertos=percentual_acertos)
-
 
     pdf_path = f'certificado_{nome.replace(" ", "_")}.pdf'
     pdfkit.from_string(rendered, pdf_path, options={'enable-local-file-access': ''})
